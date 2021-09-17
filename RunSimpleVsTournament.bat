0240f7baf6c158095d8716faab2c70719127767f
OUT-Vito-I@Sigma.sbrf.ru <1q2w>
2021-09-17 17:15:57 +0300

    Refactored more

---
+++ RunSimpleVsTournament.bat
@@ -0,0 +1,1 @@
+lux-ai-2021 --rankSystem="trueskill" --tournament --storeReplay=false --storeLogs=false main.py 
--- agent.py
+++ agent.py
@@ -27,34 +27,26 @@ def agent(observation, configuration):
     player = game_state.players[observation.player]
     opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
+    resource_tiles = find_resources(game_state)
 
-    resource_tiles: list[Cell] = []
-    for y in range(height):
-        for x in range(width):
-            cell = game_state.map.get_cell(x, y)
-            if cell.has_resource():
-                resource_tiles.append(cell)
+    cell = resource_tiles[0]
+    print("Cell at", cell.pos, "has")
+    print(cell.resource.type, cell.resource.amount)
 
     # we iterate over all our units and do something with them
     for unit in player.units:
         if unit.is_worker() and unit.can_act():
-            closest_dist = math.inf
-            closest_resource_tile = None
             if unit.get_cargo_space_left() > 0:
                 # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
-                for resource_tile in resource_tiles:
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM and not player.researched_uranium(): continue
-                    dist = resource_tile.pos.distance_to(unit.pos)
-                    if dist < closest_dist:
-                        closest_dist = dist
-                        closest_resource_tile = resource_tile
+                closest_resource_tile = find_closest_resources(unit.pos, player, resource_tiles)
                 if closest_resource_tile is not None:
+                    print("Closest resource at", closest_resource_tile.pos, "has")
+                    print(closest_resource_tile.resource.type, closest_resource_tile.resource.amount)
                     actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
             else:
                 # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
                 if len(player.cities) > 0:
-                    closest_city_tile = getClosestCity(player, unit.pos)
+                    closest_city_tile = find_closest_city_tile(player, unit.pos)
                     if closest_city_tile is not None:
                         move_dir = unit.pos.direction_to(closest_city_tile.pos)
                         actions.append(unit.move(move_dir))
@@ -65,7 +57,7 @@ def agent(observation, configuration):
     return actions
 
 
-def getClosestCity(player, pos):
+def find_closest_city_tile(player, pos):
     closest_dist = math.inf
     closest_city_tile = None
     for k, city in player.cities.items():
@@ -75,3 +67,28 @@ def getClosestCity(player, pos):
                 closest_dist = dist
                 closest_city_tile = city_tile
     return closest_city_tile
+
+# this snippet finds all resources stored on the map and puts them into a list so we can search over them
+def find_resources(game_state):
+    resource_tiles: list[Cell] = []
+    width, height = game_state.map_width, game_state.map_height
+    for y in range(height):
+        for x in range(width):
+            cell = game_state.map.get_cell(x, y)
+            if cell.has_resource():
+                resource_tiles.append(cell)
+    return resource_tiles
+
+# the next snippet finds the closest resources that we can mine given position on a map
+def find_closest_resources(pos, player, resource_tiles):
+    closest_dist = math.inf
+    closest_resource_tile = None
+    for resource_tile in resource_tiles:
+        # we skip over resources that we can't mine due to not having researched them
+        if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
+        if resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM and not player.researched_uranium(): continue
+        dist = resource_tile.pos.distance_to(pos)
+        if dist < closest_dist:
+            closest_dist = dist
+            closest_resource_tile = resource_tile
+    return closest_resource_tile
