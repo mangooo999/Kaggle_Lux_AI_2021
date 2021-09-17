9eb797bc0f980ab9caa71e60bd657835bacbab29
OUT-Vito-I@Sigma.sbrf.ru <1q2w>
2021-09-17 17:17:07 +0300

    Used template from tutorial

--- agent.py
+++ agent.py
@@ -1,72 +1,14 @@
-import math, sys
+# for kaggle-environments
 from lux.game import Game
 from lux.game_map import Cell, RESOURCE_TYPES
 from lux.constants import Constants
 from lux.game_constants import GAME_CONSTANTS
 from lux import annotate
-
-DIRECTIONS = Constants.DIRECTIONS
-game_state = None
-
-
-def agent(observation, configuration):
-    global game_state
-
-    ### Do not edit ###
-    if observation["step"] == 0:
-        game_state = Game()
-        game_state._initialize(observation["updates"])
-        game_state._update(observation["updates"][2:])
-        game_state.id = observation.player
-    else:
-        game_state._update(observation["updates"])
-    
-    actions = []
-
-    ### AI Code goes down here! ### 
-    player = game_state.players[observation.player]
-    opponent = game_state.players[(observation.player + 1) % 2]
-    width, height = game_state.map.width, game_state.map.height
-    resource_tiles = find_resources(game_state)
-
-    cell = resource_tiles[0]
-    print("Cell at", cell.pos, "has")
-    print(cell.resource.type, cell.resource.amount)
-
-    # we iterate over all our units and do something with them
-    for unit in player.units:
-        if unit.is_worker() and unit.can_act():
-            if unit.get_cargo_space_left() > 0:
-                # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
-                closest_resource_tile = find_closest_resources(unit.pos, player, resource_tiles)
-                if closest_resource_tile is not None:
-                    print("Closest resource at", closest_resource_tile.pos, "has")
-                    print(closest_resource_tile.resource.type, closest_resource_tile.resource.amount)
-                    actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
-            else:
-                # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
-                if len(player.cities) > 0:
-                    closest_city_tile = find_closest_city_tile(player, unit.pos)
-                    if closest_city_tile is not None:
-                        move_dir = unit.pos.direction_to(closest_city_tile.pos)
-                        actions.append(unit.move(move_dir))
-
-    # you can add debug annotations using the functions in the annotate object
-    # actions.append(annotate.circle(0, 0))
-    
-    return actions
+import math
+import sys
 
 
-def find_closest_city_tile(player, pos):
-    closest_dist = math.inf
-    closest_city_tile = None
-    for k, city in player.cities.items():
-        for city_tile in city.citytiles:
-            dist = city_tile.pos.distance_to(pos)
-            if dist < closest_dist:
-                closest_dist = dist
-                closest_city_tile = city_tile
-    return closest_city_tile
+### Define helper functions
 
 # this snippet finds all resources stored on the map and puts them into a list so we can search over them
 def find_resources(game_state):
@@ -78,6 +20,7 @@ def find_resources(game_state):
             if cell.has_resource():
                 resource_tiles.append(cell)
     return resource_tiles
+
 
 # the next snippet finds the closest resources that we can mine given position on a map
 def find_closest_resources(pos, player, resource_tiles):
@@ -92,3 +35,64 @@ def find_closest_resources(pos, player, resource_tiles):
             closest_dist = dist
             closest_resource_tile = resource_tile
     return closest_resource_tile
+
+
+def find_closest_city_tile(pos, player):
+    closest_city_tile = None
+    if len(player.cities) > 0:
+        closest_dist = math.inf
+        # the cities are stored as a dictionary mapping city id to the city object, which has a citytiles field that
+        # contains the information of all citytiles in that city
+        for k, city in player.cities.items():
+            for city_tile in city.citytiles:
+                dist = city_tile.pos.distance_to(pos)
+                if dist < closest_dist:
+                    closest_dist = dist
+                    closest_city_tile = city_tile
+    return closest_city_tile
+
+
+game_state = None
+
+
+def agent(observation, configuration):
+    global game_state
+
+    ### Do not edit ###
+    if observation["step"] == 0:
+        game_state = Game()
+        game_state._initialize(observation["updates"])
+        game_state._update(observation["updates"][2:])
+        game_state.id = observation.player
+    else:
+        game_state._update(observation["updates"])
+
+    actions = []
+
+    ### AI Code goes down here! ### 
+    player = game_state.players[observation.player]
+    opponent = game_state.players[(observation.player + 1) % 2]
+    width, height = game_state.map.width, game_state.map.height
+
+    resource_tiles = find_resources(game_state)
+
+    for unit in player.units:
+        # if the unit is a worker (can mine resources) and can perform an action this turn
+        if unit.is_worker() and unit.can_act():
+            # we want to mine only if there is space left in the worker's cargo
+            if unit.get_cargo_space_left() > 0:
+                # find the closest resource if it exists to this unit
+                closest_resource_tile = find_closest_resources(unit.pos, player, resource_tiles)
+                if closest_resource_tile is not None:
+                    # create a move action to move this unit in the direction of the closest resource tile and add to our actions list
+                    action = unit.move(unit.pos.direction_to(closest_resource_tile.pos))
+                    actions.append(action)
+            else:
+                # find the closest citytile and move the unit towards it to drop resources to a citytile to fuel the city
+                closest_city_tile = find_closest_city_tile(unit.pos, player)
+                if closest_city_tile is not None:
+                    # create a move action to move this unit in the direction of the closest resource tile and add to our actions list
+                    action = unit.move(unit.pos.direction_to(closest_city_tile.pos))
+                    actions.append(action)
+
+    return actions
