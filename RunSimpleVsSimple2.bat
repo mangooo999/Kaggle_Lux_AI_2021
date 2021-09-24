7ebb8edfff28df03458006d17be84c20cfbf4312
OUT-Vito-I@Sigma.sbrf.ru <1q2w>
2021-09-24 15:37:51 +0300

    Scripts

--- RunSimpleVsSimple2.bat
+++ RunSimpleVsSimple2.bat
@@ -1,1 +1,1 @@
-lux-ai-2021 rule15/main.py rule16/main.py --out=replay.json > luc-ai-2021.log
+lux-ai-2021 rule15/main.py rule16/main.py --seed 18 --out=replay.json > luc-ai-2021.log
--- RunSimpleVsSimpleLoop.bat
+++ RunSimpleVsSimpleLoop.bat
@@ -1,10 +1,11 @@
-rem echo off
+@echo off
 set agent1=rule15/main.py
 set agent2=rule16/main.py
+set num_loops=1000
 SetLocal EnableDelayedExpansion
 
 echo start %DATE% %TIME% > results.log
-FOR /L %%G IN (1,1,2) DO (
+FOR /L %%G IN (1,18,%num_loops%) DO (
 	echo|set /p="!TIME! %%G a" >> results.log
 	lux-ai-2021 --seed %%G --loglevel 1 %agent1% %agent2% | grep "rank: 1" >> results.log  
 	echo|set /p="!TIME! %%G b" >> results.log
@@ -14,7 +15,7 @@ FOR /L %%G IN (1,1,2) DO (
 echo end %DATE% %TIME% >> results.log
 
 echo|set /p="%agent1% win " >> results.log
-grep --count %agent1% results.log >> results.log
+grep --count %agent1% results.log | tee -a results.log
 
 echo|set /p="%agent2% win " >> results.log
-grep --count %agent2% results.log >> results.log
+grep --count %agent2% results.log | tee -a results.log
