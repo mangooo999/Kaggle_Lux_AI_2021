919ecf8992b72b12ea0736cfa4ee1021a2d7ee53
OUT-Vito-I@Sigma.sbrf.ru <1q2w>
2021-09-24 15:15:39 +0300

    Fixed

--- RunSimpleVsSimpleLoop.bat
+++ RunSimpleVsSimpleLoop.bat
@@ -1,12 +1,20 @@
-rem for run in {1..500}; do lux-ai-2021 --seed $run --loglevel 1 rule15/main.py rule16/main.py | tee logs.txt; done
 rem echo off
-echo start %DATE% %TIME% > results.log
+set agent1=rule15/main.py
+set agent2=rule16/main.py
+SetLocal EnableDelayedExpansion
 
-FOR /L %%G IN (1,1,3) DO (
-	echo|set /p="%TIME% %%G a" >> results.log
-	lux-ai-2021 --seed %%G --loglevel 1 rule15/main.py rule16/main.py | grep "rank: 1" >> results.log  
-	echo|set /p="%TIME% %%G b" >> results.log
-	lux-ai-2021 --seed %%G --loglevel 1 rule16/main.py rule15/main.py | grep "rank: 1" >> results.log  	
+echo start %DATE% %TIME% > results.log
+FOR /L %%G IN (1,1,2) DO (
+	echo|set /p="!TIME! %%G a" >> results.log
+	lux-ai-2021 --seed %%G --loglevel 1 %agent1% %agent2% | grep "rank: 1" >> results.log  
+	echo|set /p="!TIME! %%G b" >> results.log
+	lux-ai-2021 --seed %%G --loglevel 1 %agent2% %agent1% | grep "rank: 1" >> results.log  	
 )
 
 echo end %DATE% %TIME% >> results.log
+
+echo|set /p="%agent1% win " >> results.log
+grep --count %agent1% results.log >> results.log
+
+echo|set /p="%agent2% win " >> results.log
+grep --count %agent2% results.log >> results.log
